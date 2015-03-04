682ad9371a99ff09325d3f0dcac9165d5e888d53
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-03-03 22:13:43 -0500

    password frequency graphs

--- passwordfrequency.py
+++ passwordfrequency.py
@@ -1,5 +1,11 @@
 
 import string
+import heapq
+import operator
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def get_word_list(file_name):
     """ Creates a dictionary of passwords and their counts.
@@ -8,7 +14,6 @@ def get_word_list(file_name):
     """
     f = open(file_name,'r')
     count = dict()
-    all_words = []
     lines = f.readlines()
 
     for line in lines:
@@ -20,6 +25,19 @@ def get_word_list(file_name):
         else:
             count[words_in_line[1]] += 1        
     return count
+
+
+def num_passwords(file_name):
+    f = open(file_name,'r')
+    words = []
+    lines = f.readlines()
+    for line in lines:
+        words_in_line = string.split(line)
+        if len(words_in_line) == 1:
+            pass
+        else:
+            words.append(words_in_line[1])
+    return len(words)
 
 
 def get_top_n_words(word_list, n):
@@ -34,6 +52,37 @@ def get_top_n_words(word_list, n):
     sort = sorted(word_list, key = word_list.get, reverse = True)
     return sort[:n]
 
+def get_top_n_values(word_list, n):
+    top_words = dict(sorted(word_list.iteritems(), key=operator.itemgetter(1), reverse=True)[:n])
+    top_values = top_words.values()
+    top_values = sorted(top_values, reverse = True)
+    return top_values
+
+
+def get_percent(num_passwords, top_values):
+    percents = []
+    for value in top_values:
+        percents.append((float(value)/num_passwords)*100)
+    return percents
+
+def graph_percents(top_words, percents, top_values):
+    y_pos = np.arange(len(top_words))
+    performance = 3 + 10 * np.random.rand(len(top_words))
+
+    plt.barh(y_pos, percents)
+    plt.yticks(y_pos, top_words)
+    plt.xlabel('Percentage')
+    plt.title('Percent Apperance of Common Passwords')
+
+    #plt.show()
+    plt.savefig("percentsofcommon.png")
+
 
 counts = get_word_list("10-million-combos.txt")
-print get_top_n_words(counts, 25)
+top_values =  get_top_n_values(counts, 25)
+num_passwords = num_passwords("10-million-combos.txt")
+top_words = get_top_n_words(counts, 25)
+#print top_values
+#print top_words
+percents = get_percent(num_passwords, top_values)
+graph_percents(top_words, percents, top_values)
