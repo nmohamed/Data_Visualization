17fdfb03de093692a125b186336254fd73eb7c8c
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-03-05 00:01:32 -0500

    finished more fuctions

--- datafunctions.py
+++ datafunctions.py
@@ -3,7 +3,7 @@ import heapq
 import operator
 #import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from Levenshtein import distance
 
 
@@ -37,8 +37,23 @@ def get_word_list(file_name):
     return count
 
 def passwords_as_list(pass_dict):
-    pass_list = pass_dict.items()
-    
+    """ Turns dictionary of passwords keys to a list 
+    returns a list of passwords"""
+    pass_list = list(pass_dict.keys())
+    return pass_list
+
+def total_password_list():
+    """ return the dictionary as a list where every password is its own entry"""
+    f = open("10-million-combos.txt", 'r')
+    total_pass_list = []
+    lines = f.readlines()
+    for line  in lines:
+        words_in_line = string.split(line)
+        if len(words_in_line) == 1 or words_in_line == []:
+            pass
+        else:
+            total_pass_list.append(words_in_line[1])
+    return total_pass_list    
 
 def num_passwords(file_name):
     """ returns number of passwords"""
@@ -126,6 +141,14 @@ def find_distance(s1, s2):
 
 
 def distance_from_words(your_word, english_words):
+    """
+    your_word: string that is your password
+    english_words: list of english words
+    Uses Levenshtein distance to find the number of charecters off a password
+    is from english words and returns a list where 0 is the number of words that are 0
+    off, distance[1] is words that are 1 of etc
+    returns list
+    """
     distance = [0]*10
     for word in english_words:
         dis = find_distance(your_word, word)
@@ -134,10 +157,25 @@ def distance_from_words(your_word, english_words):
     print distance
         
 
+def distance_from_passwords(your_word, passwords):
+    """ 
+    your_word: string that is your password
+    passwordsr: list of passwords, all dublicates included 
+    Uses Levenshtein distance to find the number of charecters off a password
+    is from passwords and returns a list where 0 is the number of words that are 0
+    off, distance[1] is words that are 1 of etc
+    returns list """ 
+    distance = [0]*10
+    for word in passwords:
+        dis = find_distance(your_word, word)
+        if dis < 10:
+            distance[dis] += 1
+    print distance
 
 if __name__ == '__main__':
     pass_dict = get_word_list("10-million-combos.txt")
     english = english_words()
+    total_passwords = total_password_list()
     # top_values =  get_top_n_values(pass_dict, 25)
     # num_passwords = num_passwords("10-million-combos.txt")
     # top_words = get_top_n_words(pass_dict, 25)
@@ -147,4 +185,5 @@ if __name__ == '__main__':
     #print freq_list
     #find_distance("hello", "jello")
     distance_from_words("hello", english)
+    distance_from_passwords("hello", total_passwords)
     # print english
