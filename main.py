39d948d1ec421aa1096986c2df57a23dbc0d2c0c
Nora Mohamed <jurukix@gmail.com>
2015-03-04 20:12:02 -0500

    Added compare_to_english

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ Lucy Wilcox & Nora Mohamed
 Software Design SP2015
 """
 
-import bokeh
+#import bokeh
+from random import choice
+
+def get_word_frequency():
+	pass
+
+def get_top_n_words():
+	pass
+
+def get_text_file(file_name):
+	""" Returns text file in list of words
+	"""
+	pass
+
+combo = choice(open("test_data.txt").readlines())
 
 #main
--- passwordfrequency.py
+++ passwordfrequency.py
@@ -13,7 +13,7 @@ def get_word_list(file_name):
 
     for line in lines:
         words_in_line = string.split(line)
-        if len(words_in_line) == 1:
+        if len(words_in_line) == 1 or words_in_line == []:
             pass
         elif words_in_line[1] not in count:
             count[words_in_line[1]] = 1
@@ -34,6 +34,35 @@ def get_top_n_words(word_list, n):
     sort = sorted(word_list, key = word_list.get, reverse = True)
     return sort[:n]
 
+def compare_to_english(password):
+    """ Compare input password to english words and see if password is english words
+        or if it contains one
+        Returns string stating "Not English" or "Contains English"
+        >>> compare_to_english('friend')
+        'Contains English'
+    """
+    f = open('/usr/share/dict/american-english', 'r')
+    words = f.readlines()
+    english = []
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'n', 'o', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'es', 'as', 'so']
+    for line in words:
+        if line.translate(None, '\n') in letters:
+            pass
+        else:
+            english.append(line.translate(None, '\n'))
 
-counts = get_word_list("10-million-combos.txt")
-print get_top_n_words(counts, 25)
+    matching = [word for word in english if word in password]
+    if matching == []:
+        return "Does not contain English"
+    else:
+        return "Contains English"
+
+
+
+if __name__ == '__main__':
+    counts = get_word_list('test_data.txt')
+    top_pw = get_top_n_words(counts, 10)
+    english = compare_to_english('qwrt')
+    print english
+    #most common words, is it english
