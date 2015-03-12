98e99afb19c59507a0ce9c6bdb6a75658275ef9e
Nora Mohamed <jurukix@gmail.com>
2015-03-11 21:30:54 -0400

    dotgraphs works for top25 pw, also pushing saved html file of top25

--- dot_graph.py
+++ dot_graph.py
@@ -5,18 +5,19 @@ from datafunctions import *
 
 class DotGraph(object):
     """Used for making Bokeh dot graphs. The y axis is strings, x axis is numbers"""
-    def __init__(self, file_name, titles, your_password = ''):
-
+    def __init__(self, file_name, titles):
+        self.titles = titles
         self.p1 = figure(title= titles, tools="resize, wheel_zoom, reset, save",)
                          #y_range= y_value, x_range=[0, 1.1*x_length])
         output_file(file_name, title= titles)
-        self.x0 = [0, 0, 0, 0, 0, 0, 0, 0]
-        self.password = your_password
+        self.x0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
         passwords = File('passwords', "10-million-combos.txt", 1)
         #passwords.distance_from_list(password)
 
     def do_type_english(self, your_password):
         """ Makes a graph """
+        self.password = your_password
         passwords.counts_dictionary()
         english = English()
 
@@ -31,13 +32,13 @@ class DotGraph(object):
         passwords.get_top_n_words()
 
         self.makegraph(passwords.top_values, passwords.top_words,
-                       len(passwords.top_values), len(passwords.top_words))
+                       max(passwords.top_values), len(passwords.top_words))
 
     def makegraph(self, x_value, y_value, x_length, y_length):
         """y_value is a list of y values, x_value is list of x values"""
         self.p1.y_range = Range1d(start = 0, end = y_length)
         self.p1.x_range = Range1d(start = 0, end = 1.1*x_length)
-        self.p1 = figure(title="Dot Plot", tools="resize, wheel_zoom, reset, save",
+        self.p1 = figure(title = self.titles, tools="resize, wheel_zoom, reset, save",
                          y_range= y_value, x_range=[0, 1.1*x_length])
 
         self.p1.segment(self.x0, y_value, x_value, y_value, line_width=2, line_color="green")
@@ -49,12 +50,12 @@ class DotGraph(object):
         self.p1.yaxis.axis_label = y#'English words in your password'
 
 
-g = DotGraph('englishinyourpw-to-10mill.html', 'How Common English Words In Your Password Are')
-g.do_type_english('thisispw')
-g.make_axis_label('Number of appearances in all passwords', 'English words in your password')
-show(g.p1)
+#g = DotGraph('englishinyourpw-to-10mill.html', 'How Common English Words In Your Password Are')
+#g.do_type_english('thisispw')
+#g.make_axis_label('Number of appearances in all passwords', 'English words in your password')
+#show(g.p1)
 
-g2 = DotGraph('top25-common.html', '25 Most Common Passwords')
-g2.do_type_top25()
-g2.make_axis_label('Appearances out of 10 million', 'Most common passwords')
-show(g2.p1)
+# g2 = DotGraph('top25-common.html', '25 Most Common Passwords')
+# g2.do_type_top25()
+# g2.make_axis_label('Appearances out of 10 million', 'Most common passwords')
+# show(g2.p1)
